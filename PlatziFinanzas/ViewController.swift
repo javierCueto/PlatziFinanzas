7f56816d0df06d1934960adf88893bda53b9b4ec
Javier <javelie@outlook.com>
2020-02-10 19:57:24 -0700

    Initial Commit

---
+++ PlatziFinanzas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlatziFinanzas
+//
+//  Created by José Javier Cueto Mejía on 10/02/20.
+//  Copyright © 2020 José Javier Cueto Mejía. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
