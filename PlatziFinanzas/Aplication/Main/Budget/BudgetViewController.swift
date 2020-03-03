35092d7eaa2ae7f7b770711e0838024372a71251
Javier <javelie@outlook.com>
2020-03-02 19:34:32 -0700

    animations header

--- PlatziFinanzas/Aplication/Main/Budget/BudgetViewController.swift
+++ PlatziFinanzas/Aplication/Main/Budget/BudgetViewController.swift
@@ -12,6 +12,7 @@ class BudgetViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
+    @IBOutlet weak var animationLayout: NSLayoutConstraint!
     @IBOutlet var animationButtons: [UIButton]!
     
     override func viewDidLoad() {
@@ -22,7 +23,16 @@ class BudgetViewController: UIViewController {
     
     
     @IBAction func animateHeader(sender: UIButton){
-        
+        animationLayout.constant = sender.frame.origin.x
+        UIView.animate(withDuration: 0.5, animations: {
+           self.view.layoutIfNeeded()
+        }, completion: { (completed) in
+            self.animationButtons.forEach {
+                $0.setTitleColor(UIColor(named: "TextColor"), for: .normal)
+                sender.setTitleColor(UIColor.white, for: .normal)
+            }
+        })
+
     }
     
 }
